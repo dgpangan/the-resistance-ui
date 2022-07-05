05d11615a80a20c6c451402685865d2df7f388ae
Darren Pangan <darren@boost.co.nz>
2022-07-05 15:47:00 +0800

    feat: create TeamSelect component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <the-board></the-board>
-  <card-draw></card-draw>
+  <card-draw v-if="showDialog==='card-draw'"></card-draw>
+  <team-select v-if="showDialog==='team-select'"></team-select>
 </template>
 
 <script>
 import TheBoard from './components/TheBoard.vue';
 import CardDraw from './components/CardDraw.vue';
+import TeamSelect from './components/TeamSelect.vue';
 
 export default {
-  components: { TheBoard, CardDraw }
+  components: { TheBoard, CardDraw, TeamSelect },
+
+  data() {
+    return {
+      showDialog: 'team-select'
+    }
+  }
 }
 </script>
---
+++ src/components/TeamSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <dialog open>
+    <h2>Select X members that you would like to go on this mission:</h2>
+    <ul>
+      <li v-for="player in players" :key="player.id">{{ player.name }}</li>
+    </ul>
+  </dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      players: [
+        { id: 0, name: 'Player 1', isResistance: true },
+        { id: 1, name: 'Player 2', isResistance: true },
+        { id: 2, name: 'Player 3', isResistance: true },
+        { id: 3, name: 'Player 4', isResistance: false },
+        { id: 4, name: 'Player 5', isResistance: false },
+      ]
+    }
+  }
+}
+</script>
